1182b78aac027a7f63e6fd5b16b74306a6575626
fan cui <fan.cui@ssd3.dbg.private>
2022-09-21 21:03:18 +0800

    added comments

--- concurrency_control/silo.cpp
+++ concurrency_control/silo.cpp
@@ -38,8 +38,8 @@ txn_man::validate_silo()
 	ts_t max_tid = 0;
 	bool done = false;
 	if (_pre_abort) {
-		// Early abort a transaction if any tuple in the read set or the write set
-		// has been modified by other transactions.
+		// Early abort a transaction if any tuple in the read set or the write set has
+		// been modified by other transactions.
 		// Why check the write set?
 		for (int i = 0; i < wr_cnt; i++) {
 			row_t * row = accesses[ write_set[i] ]->orig_row;
@@ -59,6 +59,9 @@ txn_man::validate_silo()
 
 	// Lock all rows in the write set.
 	if (_validation_no_wait) {
+		// Transactions are not allowed to wait for locks. Whenever a transaction encounters
+		// a row that is locked by other transactions, it release all locks and performs an
+		// early abort check. This is deployed to avoid lock thrashing.
 		while (!done) {
 			num_locks = 0;
 			for (int i = 0; i < wr_cnt; i++) {
@@ -99,6 +102,7 @@ txn_man::validate_silo()
 			}
 		}
 	} else {
+		// Transactions wait when the required row is currently locked by other transactions.
 		for (int i = 0; i < wr_cnt; i++) {
 			row_t * row = accesses[ write_set[i] ]->orig_row;
 			row->manager->lock();
@@ -110,8 +114,8 @@ txn_man::validate_silo()
 		}
 	}
 
-	// validate rows in the read set
-	// for repeatable_read, no need to validate the read set.
+	// Validate rows in the read set.
+	// For repeatable_read, no need to validate the read set.
 	for (int i = 0; i < row_cnt - wr_cnt; i ++) {
 		Access * access = accesses[ read_set[i] ];
 		bool success = access->orig_row->manager->validate(access->tid, false);
@@ -122,8 +126,9 @@ txn_man::validate_silo()
 		if (access->tid > max_tid)
 			max_tid = access->tid;
 	}
-	// validate rows in the write set
-	for (int i = 0; i < wr_cnt; i++) {
+
+	// Validate rows in the write set.
+	for (int i = 0; i < wr_cnt; i ++) {
 		Access * access = accesses[ write_set[i] ];
 		bool success = access->orig_row->manager->validate(access->tid, true);
 		if (!success) {
@@ -133,10 +138,15 @@ txn_man::validate_silo()
 		if (access->tid > max_tid)
 			max_tid = access->tid;
 	}
+
+	// _cur_tid is the tid of the last transaction of the thread. If max_tid is bigger
+	// than _cur_tid, _cur_tid needs to be advanced. Otherwise, just increment _cur_tid
+	// to distinguish it from the last transaction.
 	if (max_tid > _cur_tid)
 		_cur_tid = max_tid + 1;
 	else 
 		_cur_tid ++;
+
 final:
 	if (rc == Abort) {
 		for (int i = 0; i < num_locks; i++) 
@@ -145,8 +155,7 @@ final:
 	} else {
 		for (int i = 0; i < wr_cnt; i++) {
 			Access * access = accesses[ write_set[i] ];
-			access->orig_row->manager->write( 
-				access->data, _cur_tid );
+			access->orig_row->manager->write( access->data, _cur_tid );
 			accesses[ write_set[i] ]->orig_row->manager->release();
 		}
 		cleanup(rc);
